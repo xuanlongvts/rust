56ab0d324a7fa194212c4917a7d67087ccadd1a1
LongLe <xuanlongvts@gmail.com>
2021-09-14 16:21:18 +0700

    tokie spawn

---
+++ tokio/my-redis/examples/hello-redis.rs
@@ -0,0 +1,46 @@
+use mini_redis::{Connection, Frame};
+use tokio::net::{TcpListener, TcpStream};
+
+#[tokio::main]
+async fn main() {
+	let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+	loop {
+		let (socket, _) = listener.accept().await.unwrap();
+		tokio::spawn(async move {
+			process(socket).await;
+		});
+	}
+}
+
+async fn process(socket: TcpStream) {
+	use mini_redis::Command::{self, Get, Set};
+	use std::collections::HashMap;
+
+	// A hashmap is used to store data
+	let mut db = HashMap::new();
+
+	// Connection, provided by `mini-redis`, handles parsing frames from the socket
+	let mut connection = Connection::new(socket);
+
+	while let Some(frame) = connection.read_frame().await.unwrap() {
+		let res = match Command::from_frame(frame).unwrap() {
+			Set(cmd) => {
+				println!("cmd: {:#?}", cmd);
+				// The value is stored as `Vec<u8>`
+				db.insert(cmd.key().to_string(), cmd.value().to_vec());
+				Frame::Simple("OK".to_string())
+			}
+			Get(cmd) => {
+				if let Some(value) = db.get(cmd.key()) {
+					Frame::Bulk(value.clone().into())
+				} else {
+					Frame::Null
+				}
+			}
+			cmd => panic!("unimplemented {:?}", cmd),
+		};
+
+		connection.write_frame(&res).await.unwrap();
+	}
+}
