4ffd0b9f74b125cf600658dd91bee1078184f2ca
LongLe <xuanlongvts@gmail.com>
2021-09-18 17:57:12 +0700

    Generic for any type search split

--- crust_of_rust/01_lifetime_annotations/strsplit/src/lib.rs
+++ crust_of_rust/01_lifetime_annotations/strsplit/src/lib.rs
@@ -1,31 +1,75 @@
+// str -> [char]
+// &str -> &[char]
+// String -> vec![char]
+
+// have a String -> can get &str (cheap -- AsRef)
+// have a &str -> can get String (expensive -- memCpy)
+
 #[derive(Debug)]
-pub struct StrSplit<'a> {
-    remainder: Option<&'a str>,
-    delimiter: &'a str,
+pub struct StrSplit<'haystack, D> {
+    remainder: Option<&'haystack str>,
+    delimiter: D,
 }
 
-impl<'a> StrSplit<'a> {
-    pub fn new(haystack: &'a str, delimiter: &'a str) -> Self {
-        // let transform_text = haystack.trim();
+impl<'haystack, D> StrSplit<'haystack, D> {
+    pub fn new(haystack: &'haystack str, delimiter: D) -> Self {
+        let trim_text = haystack.trim();
         Self {
-            remainder: Some(haystack),
+            remainder: Some(trim_text),
             delimiter,
         }
     }
 }
 
-impl<'a> Iterator for StrSplit<'a> {
-    type Item = &'a str;
+pub trait Delimiter {
+    fn find_next(&self, s: &str) -> Option<(usize, usize)>;
+}
+
+impl Delimiter for &str {
+    fn find_next(&self, s: &str) -> Option<(usize, usize)> {
+        s.find(self).map(|start| (start, start + self.len()))
+    }
+}
+
+impl Delimiter for char {
+    fn find_next(&self, s: &str) -> Option<(usize, usize)> {
+        s.char_indices()
+            .find(|(_, c)| c == self)
+            .map(|(start, _)| (start, start + self.len_utf8()))
+    }
+}
+
+impl<'haystack, D> Iterator for StrSplit<'haystack, D>
+where
+    D: Delimiter,
+{
+    type Item = &'haystack str;
     fn next(&mut self) -> Option<Self::Item> {
         let remainder = self.remainder.as_mut()?;
-        if let Some(next_delimiter) = remainder.find(self.delimiter) {
-            let until_delimiter = &remainder[..next_delimiter];
-            *remainder = &remainder[(next_delimiter + self.delimiter.len())..];
+        if let Some((delim_start, delim_end)) = self.delimiter.find_next(remainder) {
+            let until_delimiter = &remainder[..delim_start];
+            *remainder = &remainder[delim_end..];
             Some(until_delimiter)
         } else {
             self.remainder.take()
         }
     }
+}
+
+fn until_char(s: &str, c: char) -> &str {
+    // let delimiter = format!("{}", c);
+    // StrSplit::new(s, &*delimiter)
+    //     .next()
+    //     .expect("StrSplit always get at least on result")
+
+    StrSplit::new(s, c)
+        .next()
+        .expect("StrSplit always get at least on result")
+}
+
+#[test]
+fn until_char_test() {
+    assert_eq!(until_char("hello world", 'o'), "hell");
 }
 
 #[test]
